3d750ec3153720f09acaad8803b4bb5078d6558d
Jessica Aissa <jessica.aissa@live.com>
2025-11-09 09:44:33 +0100

    feat: 🎸 display duration with horus

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,5 +1,19 @@
+import dayjs from "dayjs";
+import duration from "dayjs/plugin/duration";
+
+dayjs.extend(duration);
+
 export function formatDuration(ms: number): string {
-  const minutes = Math.floor(ms / 60000);
-  const seconds = Math.floor((ms % 60000) / 1000);
-  return `${minutes}:${seconds.toString().padStart(2, "0")}`;
+  const dur = dayjs.duration(ms);
+  const hours = dur.hours();
+  const minutes = dur.minutes();
+  const seconds = dur.seconds();
+
+  if (hours > 0) {
+    return `${hours}:${minutes.toString().padStart(2, "0")}:${seconds
+      .toString()
+      .padStart(2, "0")}`;
+  } else {
+    return `${minutes}:${seconds.toString().padStart(2, "0")}`;
+  }
 }
