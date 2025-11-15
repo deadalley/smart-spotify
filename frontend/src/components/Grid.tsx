72abcc69927ee52bdb64466f54ee877b418c35e2
Jessica Aissa <jessica.aissa@live.com>
2025-11-15 20:12:03 +0100

    fix: 🐛 accordion styles and grid padding

--- frontend/src/components/Grid.tsx
+++ frontend/src/components/Grid.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export function Grid({ children }: PropsWithChildren) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3">
       {children}
     </div>
   );
